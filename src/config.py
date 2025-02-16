abc45a6afc25b15ef7aca3182c19f7d189f03bf5
pooriam <pooriamred@gmail.com>
2025-02-16 17:25:43 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -58,7 +58,7 @@ class ProxyConfig:
             ChannelConfig("https://ainita.s3.eu-north-1.amazonaws.com/AinitaServer-3.csv#Pooria3"),
             ChannelConfig("https://ainita.s3.eu-north-1.amazonaws.com/AinitaServer-4.csv#Pooria4"),
             ChannelConfig("https://yebekhe.site/api/tce/api.php?id=redv2ray_channel&base64=false"),
-            ChannelConfig("https://yebekhe.site/api/tce/api.php?id=shadoowvpnn&base64=true")
+            ChannelConfig("https://raw.githubusercontent.com/4n0nymou3/ss-config-updater/refs/heads/main/configs.txt#pooria")
         ]
 
         # Remove duplicate URLs before assigning to SOURCE_URLS
